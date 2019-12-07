f30a76b8fb3548c981e6557f23b3808811ecf00a
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-07 14:45:04 +0900

    Create save_mp3.py

---
+++ code/save_mp3.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+text ="결제가 완료되었습니다."
+
+tts = gTTS(text=text, lang='en')
+tts.save("./audio/finish.mp3")
+
+text ="1번 세탁기, 가동을 시작합니다."
+
+tts = gTTS(text=text, lang='en')
+tts.save("./audio/1_run.mp3")
