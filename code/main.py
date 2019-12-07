340a840a8e4565c4339330a4267cfb6e0cdbbd15
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-07 16:15:18 +0900

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,14 +2,16 @@
 
 from pn532.api import PN532
 from pygame import mixer
+import pygame
+from time import sleep
 
 if __name__== "__main__":
     nfc = PN532()
-
     # setup the device
     nfc.setup(enable_logging=False)
 
-    # keep reading until a value is returned
+    # keep reading until a value is returned   
+    clock = pygame.time.Clock()
     while True:
         if(nfc.read()):
             read = nfc.read()
@@ -18,3 +20,11 @@ if __name__== "__main__":
             mixer.init()
             mixer.music.load('./audio/finish.mp3')
             mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                clock.tick(1000)
+            pygame.mixer.music.stop()
+            mixer.init()
+            mixer.music.load('./audio/1_run.mp3')
+            mixer.music.play()
+
+        
