ef19b5c5b06d98fc4a429effbe290fd22c2ae909
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-07 14:44:49 +0900

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from pn532.api import PN532
-
+from pygame import mixer
 
 if __name__== "__main__":
     nfc = PN532()
@@ -15,3 +15,9 @@ if __name__== "__main__":
             read = nfc.read()
             print("New NFC READ!")
             print(read)
+            mixer.init()
+            mixer.music.load('../audio/finish.mp3')
+            mixer.music.play()
+            mixer.init()
+            mixer.music.load('../audio/1_run.mp3')
+            mixer.music.play()
